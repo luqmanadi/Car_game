65f005bd0641c00d231b3d198a83a05552df98cd
AisAif <Isbahrulfahni@gmail.com>
2023-05-18 13:59:01 +0700

    Update besar-besaran

--- src/Car/CarGameGUI.java
+++ src/Car/CarGameGUI.java
@@ -1,3 +1,4 @@
+package Car;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,7 +9,7 @@
  * @author LENOVO
  */
 public class CarGameGUI {
-    public static void main(String[] args) {
+    public CarGameGUI(){
         System.out.println("Hello World");
     }
 }
---
+++ src/Driver.java
@@ -0,0 +1,7 @@
+import Car.CarGameGUI;
+
+public class Driver {
+    public static void main(String[] args) {
+        CarGameGUI cgg = new CarGameGUI();
+    }
+}
