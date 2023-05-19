57bf1d6577ecc0a97a1d7323eaaab25fb0076085
luqmanadi <78971486+luqmanadi@users.noreply.github.com>
2023-05-19 17:20:48 +0700

    Part 1

--- src/Car/CarGameGUI.java
+++ src/Car/CarGameGUI.java
@@ -1,4 +1,11 @@
 package Car;
+
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -6,8 +13,28 @@ package Car;
 
 /**
  *
- * @author LENOVO
+ * @author Kelompok 11
  */
 public class CarGameGUI {
-    
+    JFrame fr;
+    JPanel carPanel;
+    public CarGameGUI(){
+        initGUI();
+    }
+    public void initGUI(){
+        fr = new JFrame("Car Game");
+        fr.setLayout(null);
+        carPanel = new JPanel();
+        carPanel.setSize(80,183);
+        
+        JLabel car = new JLabel(new ImageIcon("ferrari5.png"));
+        carPanel.add(car);
+        carPanel.setLocation(350, 280);
+        //carPanel.setBackground(Color.blue);
+        fr.add(carPanel);
+        fr.setSize(800,500);
+        fr.setVisible(true);
+        fr.setLocationRelativeTo(null);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,13 @@
 import Car.CarGameGUI;
 
+/**
+ *
+ * @author Kelompok 11
+ */
+
 public class Driver {
-    
+    public static void main(String[] args) {
+        // membuat objek CarGameGUI
+        CarGameGUI game = new CarGameGUI();
+    }
 }
